e61f0197e8d18087ad402ce5ff6e2d161b6a82a6
sulthanfathur <90336772+sulthanfathur@users.noreply.github.com>
2022-03-16 00:50:18 +0700

    Update models.py
    added defaults for title_tag and post_snippet

--- theblog/models.py
+++ theblog/models.py
@@ -5,10 +5,10 @@ from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="Fathur's blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
-    post_snippet = models.CharField(max_length=255)
+    post_snippet = models.CharField(max_length=255, default="Click the link above!")
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
